54f39dc5695dee6d53a11f1b629ea9f36af81a80
Aleena Calvert <calveale@flip3.engr.oregonstate.edu>
2026-04-19 23:46:12 -0700

    finished revenue problem

--- revenue.cpp
+++ revenue.cpp
@@ -1,3 +1,23 @@
-int main() {
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+    double cp, b, rp;
+
+    cout << "Enter cost per device: ";
+    cin >> cp;
+
+    cout << "Enter base cost: ";
+    cin >> b;
+
+    cout << "Enter revenue per device: ";
+    cin >> rp;
+
+    double N = b / (rp - cp);
+    int devices = ceil(N);
+
+    cout << "Minimum number of devices to break even: " << devices << endl;
+
+    return 0;
 }
