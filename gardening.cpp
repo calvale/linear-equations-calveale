71e52848d23c240da8a9e374c7c0626e87a1d043
calvale <calveale@oregonstate.edu>
2026-04-19 23:55:33 -0700

    gardening.cpp

--- gardening.cpp
+++ gardening.cpp
@@ -18,30 +18,24 @@ int main() {
     cin >> k1 >> k2 >> k3;
 
     double M1, M2, M3;
-
-    // Break equations into parts (makes it easier and cleaner)
+    
     double A = (n2 - n3 + p3 - p2);
     double B = (n1 - n3 + p3 - p1);
     double C = (n3 - n2 + k2 - k3);
 
-    // Solve for M1
     double numerator = (k3 - n3 + n1 - k1) + C * (A / B);
     double denominator = (k3 - n3) + C * (A / (p3 - n3));
 
     M1 = numerator / denominator;
 
-    // Solve for M2
     M2 = A / ((p3 - n3) - M1 * B);
 
-    // Solve for M3
     M3 = 1 - M1 - M2;
 
-    // Output mixing proportions
     cout << "M1 = " << M1 << endl;
     cout << "M2 = " << M2 << endl;
     cout << "M3 = " << M3 << endl;
 
-    // Check N, P, K values
     double nitrogen = M1 * n1 + M2 * n2 + M3 * n3;
     double phosphorus = M1 * p1 + M2 * p2 + M3 * p3;
     double potassium = M1 * k1 + M2 * k2 + M3 * k3;
